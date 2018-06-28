3a43c0aca6627bfe0e83ff37b9ac51db0043a3ca
pijoneris <benasmiliunas@gmail.com>
2018-06-28 19:15:04 +0300

    slider

--- MigraineNova/app/src/main/java/com/solution/migraine/migrainenova/QuestionaireMIDAS.java
+++ MigraineNova/app/src/main/java/com/solution/migraine/migrainenova/QuestionaireMIDAS.java
@@ -3,8 +3,20 @@ package com.solution.migraine.migrainenova;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class QuestionaireMIDAS extends AppCompatActivity {
+
+    private ImageView img_arrow_back;
+    private EditText first_question_ans;
+    private EditText second_question_ans;
+    private EditText third_question_ans;
+    private EditText fourth_question_ans;
+    private EditText fifth_question_ans;
+    private EditText sixth_question_ans;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,15 @@ public class QuestionaireMIDAS extends AppCompatActivity {
     }
 
     private void initializeViews(){
+        img_arrow_back = (ImageView) findViewById(R.id.arrow_back);
+        img_arrow_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                QuestionaireMIDAS.this.finish();
+            }
+        });
+
+
 
     }
 }
